b60c08918824428df2846d18cfe42599e6da8ac1
LittleC <i@ltlec.cn>
2021-08-14 22:28:23 +0800

    :zap: update build config

--- packages/web/src/components/instance.ts
+++ packages/web/src/components/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:54000/'
+  baseURL: import.meta.env.VITE_SERVER?.toString() || 'http://localhost:54000/'
 })
 instance.interceptors.request.use((req) => {
   req.headers.authorization = `Bearer ${localStorage.getItem("token")}`
