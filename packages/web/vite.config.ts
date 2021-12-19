415b0a38c05ce3e1cf14ab268e90358c0f82c503
XxLittleCxX <i@ltlec.com>
2021-12-19 13:35:41 +0800

    :bug: wtf

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,4 +1,6 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   plugins: [reactRefresh()]
